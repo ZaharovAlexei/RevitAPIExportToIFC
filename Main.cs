e8b42023081fd03ee277a60a57462cb3aed945b9
Алексей Захаров <zakharov.nnov@gmail.com>
2022-02-08 23:14:15 +0300

    Добавьте файлы проекта.

---
+++ Main.cs
@@ -0,0 +1,31 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPIExportToIFC
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+
+            using (var ts = new Transaction(doc, "export ifc"))
+            {
+                ts.Start();
+
+                var ifcOption = new IFCExportOptions();
+                doc.Export(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "export.ifc", ifcOption);
+
+                ts.Commit();
+            }
+            return Result.Succeeded;
+        }
+    }
+}
